25c58991413cdcf66313faaac3c49173ab6fc9ea
Autumn Parker <agmparker@gmail.com>
2023-03-04 17:38:40 -0600

    Replace Starting Code
    - Replaced the starting code in App.js with a blank app
    - Includes a basic form used to add tasks

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+// Blank app with basic form to add tasks
+
+import React from 'react';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <form>
+        <input type="text" />
+        <button type="submit">Add Todo</button>
+      </form>
     </div>
   );
 }
