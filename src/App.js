87ad1d681ef453d3446e096764a20556238b5acc
Autumn Parker <agmparker@gmail.com>
2023-03-04 17:50:55 -0600

    Import useState
    - Imported useState from react
    - Created 2 state variables for our todo list and new todo list items
    - Event that updates the state variable each time the user inputs a new task in the field provided

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
 // Blank app with basic form to add tasks
+// Import useState - to hold onto list of tasks
 
-import React from 'react';
+import React, { useState } from 'react';
 
 function App() {
+  // State variable for todos list
+  const [todos, setTodos] = useState([]);
+  // State variable for new todos
+  const [newTodo, setNewTodo] = useState("");
   return (
     <div>
       <form>
-        <input type="text" />
+        {/* Updates the state varliable newTodo each time the user types and a new task */}
+        <input type="text"
+          value={newTodo}
+          onChange={(event) => setNewTodo(event.target.value)}
+        />
         <button type="submit">Add Todo</button>
       </form>
     </div>
